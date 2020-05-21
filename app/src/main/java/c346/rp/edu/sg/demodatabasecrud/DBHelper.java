a0123361143aff90c98bda4c2ede1b7b38e9b743
18039253 <18039253@myrp.edu.sg>
2020-05-21 12:32:28 +0800

    first

--- app/src/main/java/c346/rp/edu/sg/demodatabasecrud/DBHelper.java
+++ app/src/main/java/c346/rp/edu/sg/demodatabasecrud/DBHelper.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 
 public class DBHelper extends SQLiteOpenHelper {
     private static final String DATABASE_NAME = "simplenotes.db";
-    private static final int DATABASE_VERSION = 1;
+    private static final int DATABASE_VERSION = 2;
     private static final String TABLE_NOTE = "note";
     private static final String COLUMN_ID = "_id";
     private static final String COLUMN_NOTE_CONTENT = "note_content";
@@ -39,8 +39,7 @@ public class DBHelper extends SQLiteOpenHelper {
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NOTE);
-        onCreate(db);
+        db.execSQL("ALTER TABLE " + TABLE_NOTE + " ADD COLUMN module_name TEXT ");
     }
 
     public long insertNote(String noteContent){
@@ -56,12 +55,15 @@ public class DBHelper extends SQLiteOpenHelper {
         return result;
     }
 
-    public ArrayList<Note> getAllNotes(){
+    public ArrayList<Note> getAllNotes(String keyword){
         ArrayList<Note> notes = new ArrayList<Note>();
-        String selectQuery = "SELECT " + COLUMN_ID + ", " + COLUMN_NOTE_CONTENT
-                + " FROM " + TABLE_NOTE;
+
+        String[] columns = {COLUMN_ID, COLUMN_NOTE_CONTENT};
+        String condition = COLUMN_NOTE_CONTENT + "Like ?";
+        String[] args = {"%" + keyword + "%"};
+
         SQLiteDatabase db = this.getReadableDatabase();
-        Cursor cursor = db.rawQuery(selectQuery, null);
+        Cursor cursor = db.query(TABLE_NOTE, columns, condition, args,null, null, null, null);
         if(cursor.moveToFirst()){
             do{
                 int id = cursor.getInt(0);
@@ -74,4 +76,31 @@ public class DBHelper extends SQLiteOpenHelper {
         db.close();
         return notes;
     }
+
+    public int updateNote(Note data){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_NOTE_CONTENT, data.getNoteContent());
+        String condition = COLUMN_ID + " = ?";
+        String[] args = {String.valueOf(data.getId())};
+        int result = db.update(TABLE_NOTE, values, condition, args);
+        if(result < 1){
+            Log.d("DBHelper", "Update failed");
+        }
+        db.close();
+        return result;
+    }
+
+    public int deleteNote(int id){
+        SQLiteDatabase db = this.getWritableDatabase();
+        String condition = COLUMN_ID + "= ?";
+        String[] args = {String.valueOf(id)};
+        int result = db.delete(TABLE_NOTE, condition, args);
+        if(result < 1){
+            Log.d("DBHelper", "Delete failed");
+        }
+        db.close();
+        return result;
+    }
+
 }
--- app/src/main/java/c346/rp/edu/sg/demodatabasecrud/MainActivity.java
+++ app/src/main/java/c346/rp/edu/sg/demodatabasecrud/MainActivity.java
@@ -1,14 +1,112 @@
 package c346.rp.edu.sg.demodatabasecrud;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnAdd, btnEdit, btnRetrieve;
+    TextView tvDBContent;
+    EditText etContent;
+    ArrayList<Note> al;
+    ListView lv;
+    ArrayAdapter aa;
+    int requestCode = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // initialize the variables with UI here
+
+        al = new ArrayList<Note>();
+        btnAdd = (Button)findViewById(R.id.btnAdd);
+        btnEdit = (Button)findViewById(R.id.btnEdit);
+        btnRetrieve = (Button)findViewById(R.id.btnRetrieve);
+        etContent = (EditText)findViewById(R.id.etContent);
+        tvDBContent = (TextView)findViewById(R.id.tvDBContent);
+        lv = (ListView)findViewById(R.id.lv);
+        aa = new ArrayAdapter<Note>(this, android.R.layout.simple_expandable_list_item_1, al);
+        lv.setAdapter(aa);
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Note target = al.get(position);
+                Intent i = new Intent(MainActivity.this, EditActivity.class);
+                i.putExtra("data", target);
+                startActivityForResult(i, requestCode);
+            }
+        });
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String data = etContent.getText().toString();
+                DBHelper dbh = new DBHelper(MainActivity.this);
+                long insert_id = dbh.insertNote(data);
+                dbh.close();
+
+                if(insert_id != -1){
+                    Toast.makeText(MainActivity.this, "Insert successfully", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        btnRetrieve.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DBHelper dbh = new DBHelper(MainActivity.this);
+                al.clear();
+                al.addAll(dbh.getAllNotes());
+                dbh.close();
+
+                String txt = "";
+                for (int i = 0; i < al.size(); i++){
+                    Note tmp = al.get(i);
+                    txt += "ID: " + tmp.getId() + ", " + tmp.getNoteContent() + "\n";
+                }
+                tvDBContent.setText(txt);
+                aa.notifyDataSetChanged();
+            }
+        });
+
+        btnEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Note target = al.get(0);
+
+                Intent i = new Intent(MainActivity.this, EditActivity.class);
+                i.putExtra("data", target);
+                startActivityForResult(i, requestCode);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode == RESULT_OK){
+            if(data != null) {
+                DBHelper dbh = new DBHelper(MainActivity.this);
+                btnRetrieve.performClick();
+            }
+
+        }
     }
 }
